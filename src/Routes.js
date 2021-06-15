9fec3d9a695e7d77502a2bb14a24e95922449f10
Braxston <braxston.case@gmail.com>
2021-06-15 14:32:32 -0600

    Adding Auth and New notes

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import NewNote from "./containers/NewNote";
+import Signup from "./containers/Signup";
 
 export default function Routes () {
     return (
@@ -12,6 +14,12 @@ export default function Routes () {
             </Route>
             <Route exact path="/login">
                 <Login />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
+            </Route>
+            <Route exact path="/notes/new">
+                <NewNote />
             </Route>
             <Route>
                 <NotFound />
